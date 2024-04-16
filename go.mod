4bbb7194c9908bc9211f25fb7af3585ab20e47a6
Acexy <thankjava@gmail.com>
2024-04-16 14:56:29 +0800

    update lib

--- go.mod
+++ go.mod
@@ -3,16 +3,16 @@ module github.com/golang-acexy/starter-redis
 go 1.20
 
 require (
-	github.com/acexy/golang-toolkit v0.0.4
+	github.com/acexy/golang-toolkit v0.0.5
 	github.com/bsm/redislock v0.9.4
 	github.com/golang-acexy/starter-parent v0.0.5
 	github.com/redis/go-redis/v9 v9.5.1
 )
 
 require (
-	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
-	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 )
--- test/redisloader_test.go
+++ test/redisloader_test.go
@@ -22,9 +22,47 @@ func TestStandalone(t *testing.T) {
 			Addrs:    []string{":6379"},
 			Password: "tech-acexy",
 		},
-		RedisInterceptor: func(instance interface{}) {
-			c := instance.(*redis.Client)
-			fmt.Println(c.String())
+		RedisInterceptor: func(instance redis.UniversalClient) {
+			fmt.Println(instance.PoolStats())
+		},
+	}
+	moduleLoaders = []declaration.ModuleLoader{rModule}
+
+	m = declaration.Module{ModuleLoaders: moduleLoaders}
+
+	err := m.Load()
+	if err != nil {
+		fmt.Printf("%+v\n", err)
+	}
+
+	// 启动一批协程，并执行延迟sql，模拟并发多连接执行中场景
+	go func() {
+		for i := 1; i <= 10; i++ {
+			go func() {
+				for {
+					err = redismodule.Set(context.Background(), redismodule.RedisKey(random.RandString(5)), random.RandString(5))
+					if err != nil {
+						fmt.Printf("%+v", err)
+					}
+				}
+			}()
+		}
+	}()
+
+	time.Sleep(time.Second * 3)
+	fmt.Println(rModule.Unregister(10))
+}
+
+// 集群Redis
+func TestCluster(t *testing.T) {
+
+	rModule = &redismodule.RedisModule{
+		RedisConfig: &redis.UniversalOptions{
+			Addrs:    []string{":6379", ":6381", ":6380"},
+			Password: "tech-acexy",
+		},
+		RedisInterceptor: func(instance redis.UniversalClient) {
+			fmt.Println(instance.PoolStats())
 		},
 	}
 	moduleLoaders = []declaration.ModuleLoader{rModule}
